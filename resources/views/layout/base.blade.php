a4723453203bb0b4139496a00678a4c20e8ecef9
DanieleSavoia <danielesavoia44@gmail.com>
2023-07-04 15:47:33 +0200

    repo-template 2.0

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DC Comics</title>
+    <title>Laravel Invoices</title>
     @vite('resources/js/app.js')
 </head>
 
