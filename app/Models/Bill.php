d1d4e1a4b052b84c94f2216fea69b0ba3d437282
DanieleSavoia <danielesavoia44@gmail.com>
2023-07-04 15:08:00 +0200

    repo con errore

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Bill extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    protected $fillable = ['number', 'year', 'paid'];
 }
--- database/migrations/2023_07_04_103753_create_bills_table.php
+++ database/migrations/2023_07_04_103753_create_bills_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('number', 10);
             $table->year('year');
             $table->boolean('paid');
-            $table->timestamps();
         });
     }
 
